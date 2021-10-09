1543adbc764c2e15b718ad9635e9e185c66ad658
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-10-09 16:41:07 -0300

    feat(authentication): add interface IAuthentication usecase

---
+++ src/domain/usecases/Authentication.ts
@@ -0,0 +1,11 @@
+import { AccountModel } from "../models/AccountModel"
+
+type AuthenticationParams = {
+    email: string,
+    password: string
+}
+interface IAuthentication{
+    auth(params: AuthenticationParams): Promise<AccountModel>
+}
+
+export { IAuthentication };
